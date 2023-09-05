7c1aa274fffa010ff1159ef6181aea93b20a3160
Frank Hoffmann <15r10nk-git@polarbit.de>
2023-09-05 20:28:18 +0200

    feat: options for node_limit depth_limit and root_node

--- pysource_codegen/__main__.py
+++ pysource_codegen/__main__.py
@@ -6,8 +6,23 @@ from ._codegen import generate
 def run():
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", type=int, help="seed value")
+    parser.add_argument(
+        "--node-limit", type=int, default=400, help="limit number of nodes"
+    )
+    parser.add_argument(
+        "--depth-limit", type=int, default=5, help="limit for the depth of the ast"
+    )
+    parser.add_argument("--root-node", type=str, default="Module", help="root ast type")
     args = parser.parse_args()
-    print(generate(args.seed))
+
+    print(
+        generate(
+            args.seed,
+            node_limit=args.node_limit,
+            depth_limit=args.depth_limit,
+            root_node=args.root_node,
+        )
+    )
 
 
 if __name__ == "__main__":
