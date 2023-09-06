55cfd691631b643760377d81d3c94d14e17eabb1
Frank Hoffmann <15r10nk-git@polarbit.de>
2023-09-06 23:10:40 +0200

    build: use nox-poetry

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import nox
+from nox_poetry import session
 
 nox.options.sessions = ["clean", "test", "report", "mypy"]
 nox.options.reuse_existing_virtualenvs = True
@@ -8,23 +9,22 @@ nox.options.reuse_existing_virtualenvs = True
 python_versions = ["3.7", "3.8", "3.9", "3.10", "3.11"]
 
 
-@nox.session(python="python3.11")
+@session(python="python3.11")
 def clean(session):
-    session.run_always("poetry", "install", "--with=dev", external=True)
+    session.install("coverage")
     session.env["TOP"] = str(Path(__file__).parent)
     session.run("coverage", "erase")
 
 
-@nox.session(python=python_versions)
+@session(python=python_versions)
 def mypy(session):
-    session.install("poetry")
-    session.run("poetry", "install", "--with=dev")
+    session.install(".", "mypy", "pytest", "rich")
     session.run("mypy", "pysource_codegen", "tests")
 
 
-@nox.session(python=python_versions)
+@session(python=python_versions)
 def test(session):
-    session.run_always("poetry", "install", "--with=dev", external=True)
+    session.install(".", "pytest", "pytest-xdist", "rich", "coverage-enable-subprocess")
     session.env["COVERAGE_PROCESS_START"] = str(
         Path(__file__).parent / "pyproject.toml"
     )
@@ -36,9 +36,10 @@ def test(session):
     )
 
 
-@nox.session(python="python3.11")
+@session(python="python3.11")
 def report(session):
-    session.run_always("poetry", "install", "--with=dev", external=True)
+    session.install("coverage")
+
     session.env["TOP"] = str(Path(__file__).parent)
     try:
         session.run("coverage", "combine")
@@ -46,10 +47,3 @@ def report(session):
         pass
     session.run("coverage", "html")
     session.run("coverage", "report")
-
-
-# @nox.session(python="python3.10")
-# def docs(session):
-#    session.install("poetry")
-#    session.run("poetry", "install", "--with=doc")
-#    session.run("mkdocs", "build")
