57ab140e1ff8280871301740a4ca33adc63d66d0
Frank Hoffmann <15r10nk-git@polarbit.de>
2023-11-24 15:11:51 +0100

    fix: correct handling of used names in DictComp

--- pysource_codegen/_codegen.py
+++ pysource_codegen/_codegen.py
@@ -1085,6 +1085,10 @@ def fix_nonlocal(node):
             self.visit(node.generators[0].iter)
             return node
 
+        def visit_DictComp(self, node: ast.DictComp) -> Any:
+            self.visit(node.generators[0].iter)
+            return node
+
         def visit_Nonlocal(self, node: ast.Nonlocal) -> Any:
             # TODO: research __class__ seems to be defined in the class scope
             # but it is also not
