1a07b71b454550791d585ea6b90e6514ee82b065
Jan Vollmer <jan@vllmr.dev>
2025-01-04 18:42:55 +0100

    type in comment

--- src/pysource_codegen/_codegen.py
+++ src/pysource_codegen/_codegen.py
@@ -1611,7 +1611,7 @@ class AstGenerator:
                         "'\"'''\"\"\"{}\\",
                         b"'\"'''\"\"\"{}\\",
                         b"\xef\xbb\xbf",  # utf-8
-                        b"\xff\xfe\0\0",  # utf-16
+                        b"\xff\xfe\0\0",  # utf-32
                         b"\0\0\xfe\xff",  # utf-32be
                         b"\xff\xfe",  # utf-16
                         b"\xfe\xff",  # utf-16be
